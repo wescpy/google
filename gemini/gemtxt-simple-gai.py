973dc4d5255c738494d4717858585f36de49a6c1
wesley chun <wescpy@gmail.com>
2024-09-09 09:43:41 -0700

    fix typo

--- gemini/gemtxt-simple-gai.py
+++ gemini/gemtxt-simple-gai.py
@@ -19,7 +19,7 @@ import google.generativeai as genai
 from settings import API_KEY
 
 PROMPT = 'Describe a cat in a few sentences'
-const MODEL = 'gemini-1.5-flash'
+MODEL = 'gemini-1.5-flash'
 print('** GenAI text: %r model & prompt %r\n' % (MODEL, PROMPT))
 
 genai.configure(api_key=API_KEY)
